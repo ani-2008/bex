ed2c1df1ce0ef84a88d77f39125a5be4f5c54aef
bitabyte <anirudan2008@gmail.com>
2026-03-03 22:53:59 +0530

    parser

--- src/bex.c
+++ src/bex.c
@@ -3,7 +3,7 @@
 #define MAX_SIZE 4096
 
 typedef enum{
-    VAL, AND, OR, NOT
+    VAL, AND, OR, NOT, LPARA, RPARA
 }Tokens;
 
 struct Token{
@@ -13,11 +13,66 @@ struct Token{
 
 struct Token tokens[MAX_SIZE];
 int tokens_ptr = 0;
+int current_tokens_index = 0;
 
-void lexer(char *s)
+int parse_or();
+int parse_and();
+int parse_val();
+
+int parse_val()
+{
+    if(tokens[current_tokens_index].value == 0 || tokens[current_tokens_index].value == 1){
+        return tokens[current_tokens_index++].value;
+    
+    }else if(tokens[current_tokens_index].tok == 4){
+        current_tokens_index++;
+        int result = parse_or();
+        if(tokens[current_tokens_index++].tok == 5){
+            return result;
+        }
+
+    }else{
+        while(tokens[current_tokens_index].tok == 3){
+            current_tokens_index++;
+            int x = parse_val();
+            return !x;
+        }
+    }
+}
+
+
+int parse_and()
+{
+    int result = parse_val();
+    
+    while(tokens[current_tokens_index].tok == 1){
+        current_tokens_index++;
+        int next_val = parse_val();
+
+        result = result && next_val;
+    }
+
+    return result;
+}
+
+int parse_or()
+{
+    int result = parse_and();
+
+    while(tokens[current_tokens_index].tok == 2){
+        current_tokens_index++;
+        int next_val = parse_and();
+        
+        result = result || next_val;
+    }
+    return result;
+}
+
+
+int lexer(char *s)
 {
     while(*s != '\0'){
-        if(*s == ' '){
+        if(*s == ' ' || *s == '\n'){
             ;
         }else if(*s == '0' || *s == '1'){
             tokens[tokens_ptr++] = (struct Token){.tok = VAL, .value = *s - '0'};
@@ -27,9 +82,16 @@ void lexer(char *s)
             tokens[tokens_ptr++] = (struct Token){.tok = OR, .value = -1};
         }else if(*s == '~'){
             tokens[tokens_ptr++] = (struct Token){.tok = NOT, .value = -1};
+        }else if(*s == '('){
+            tokens[tokens_ptr++] = (struct Token){.tok = LPARA, .value = -1};
+        }else if(*s == ')'){
+            tokens[tokens_ptr++] = (struct Token){.tok = RPARA, .value = -1};
+        }else{
+            return -1;
         }
         s++;
     }
+    return 0;
 }
 
 int main()
@@ -38,6 +100,13 @@ int main()
     while(strcmp(buff,"quit\n")!=0 && strcmp(buff,"exit\n")!=0){
         printf("> ");
         fgets(buff,MAX_SIZE,stdin);
-        lexer(buff);
+        if(lexer(buff) == 0){
+            if(!(*buff == '\0' || *buff == '\n')){
+                int x = parse_or();
+                printf("%d\n",x);
+                tokens_ptr = 0;
+                current_tokens_index = 0;
+            }
+        }else{printf("ERROR: UNEXPECTED CHARACTER !!!!\n");}
     }
 }
